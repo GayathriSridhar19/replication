e748949713438229041a4ff5e4c1752edc8b1df4
GayathriSridhar19 <gayathripragya.vt@gmail.com>
2022-05-13 15:07:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App.css";
+import {Pricecard} from "./components/Pricecard"
+
+
 
 function App() {
+  let data = [
+    {
+      product: "User",
+      price: 0,
+      user: "Single User",
+      userEnabler: true,
+      storage: "5GB Storage",
+      storageEnabler: true,
+      publicProjects: "Unlimited Public Projects",
+      publicProjectsEnabler: true,
+      communityAccess: "Community Access",
+      communityAccessEnabler: true,
+      privateProjects: "Unlimited Private Projects",
+      privateProjectsEnabler: false,
+      phoneSupport: "Dedicated Phone Support",
+      phoneSupportEnabler: false,
+      subDomain: "Free Sub Domain",
+      subDomainEnabler: false,
+      reports: "Monthly Status Reports",
+      reportsEnabler: false,
+    },
+    {
+      product: "User",
+      price: 9,
+      user: "5 User",
+      userEnabler: true,
+      storage: "50GB Storage",
+      storageEnabler: true,
+      publicProjects: "Unlimited Public Projects",
+      publicProjectsEnabler: true,
+      communityAccess: "Community Access",
+      communityAccessEnabler: true,
+      privateProjects: "Unlimited Private Projects",
+      privateProjectsEnabler: true,
+      phoneSupport: "Dedicated Phone Support",
+      phoneSupportEnabler: true,
+      subDomain: "Free Sub Domain",
+      subDomainEnabler: true,
+      reports: "Monthly Status Reports",
+      reportsEnabler: false,
+    },
+    {
+      product: "User",
+      price: 49,
+      user: "Unlimited User",
+      userEnabler: true,
+      storage: "150GB Storage",
+      storageEnabler: true,
+      publicProjects: "Unlimited Public Projects",
+      publicProjectsEnabler: true,
+      communityAccess: "Community Access",
+      communityAccessEnabler: true,
+      privateProjects: "Unlimited Private Projects",
+      privateProjectsEnabler: true,
+      phoneSupport: "Dedicated Phone Support",
+      phoneSupportEnabler: true,
+      subDomain: "Free Sub Domain",
+      subDomainEnabler: true,
+      reports: "Monthly Status Reports",
+      reportsEnabler: true,
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="pricing py-5">
+        <div className="container">
+          <div className="row">
+            <Pricecard type={data[0]} />
+            <Pricecard type={data[1]} />
+            <Pricecard type={data[2]} />
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
-export default App;
+export default App
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
